3991f53f2b5224924954dd12c6a6a3ee7b1c65fb
star7th <xing7th@gmai.com>
2019-05-17 15:27:02 +0800

    用async/await来优化回调

---
+++ devTest.js
@@ -0,0 +1,38 @@
+/**
+ * @author  xing7th@gmail.com
+ * @website http://www.showdoc.cc/htq
+ */
+
+const express = require('express');
+const bodyParser =  require("body-parser"); 
+const redis = require('redis');
+const request = require('request');
+const asyncRedis = require("async-redis");
+const client = asyncRedis.createClient();
+const fs = require('fs');
+const fetch = require("node-fetch");
+
+const config = JSON.parse(fs.readFileSync('./config.json').toString());
+const redis_client = asyncRedis.createClient(config.redis_port,config.redis_host); //creates a new client
+console.log("后台队列服务已经启动，随时等待新队列任务");
+var queue_status_array = [];
+
+async function check_queue_hash(){
+	var reply = await redis_client.hscan(config.redis_key_prefix+"queue_list",0);
+	//console.log(reply[1]);
+	var queue_list = reply[1] ;
+
+	var cur_time = Date.parse(new Date());
+	var reply =  await redis_client.zrangebyscore(queue_list[0],0,cur_time,'LIMIT',0,1);
+	//console.log(reply);
+	var request_url = "http://127.0.0.1/test.php";
+	let response = await fetch(request_url,{ timeout : 30*1000});
+	let body = await response.text();
+	
+	console.log(body);
+}
+
+
+check_queue_hash() ;
+
+
--- queue.js
+++ queue.js
@@ -2,13 +2,13 @@
  * @author  xing7th@gmail.com
  * @website http://www.showdoc.cc/htq
  */
-var express = require('express');
-var bodyParser =  require("body-parser"); 
-var redis = require('redis');
-var request = require('request');
-var fs = require('fs');
-var config = JSON.parse(fs.readFileSync('./config.json').toString());
-var redis_client = redis.createClient(config.redis_port,config.redis_host); //creates a new client
+const asyncRedis = require("async-redis");
+const client = asyncRedis.createClient();
+const fs = require('fs');
+const fetch = require("node-fetch");
+
+const config = JSON.parse(fs.readFileSync('./config.json').toString());
+const redis_client = asyncRedis.createClient(config.redis_port,config.redis_host); //creates a new client
 console.log("后台队列服务已经启动，随时等待新队列任务");
 var queue_status_array = [];
 
@@ -19,8 +19,9 @@ setInterval(function(){
 
 
 //扫描整个队列哈希表
-function check_queue_hash(){
-	redis_client.hscan(config.redis_key_prefix+"queue_list",0,function(err, reply){
+async function check_queue_hash(){
+	var reply = await redis_client.hscan(config.redis_key_prefix+"queue_list",0);
+	if (reply && reply[1] ) {
 		var queue_list = reply[1] ;
 		if (queue_list) {
 			for (var i = 0; i < queue_list.length; i=i+2) {
@@ -31,82 +32,76 @@ function check_queue_hash(){
 					////console.log('队列'+single_queue_name+'正操作中，不重复启动');
 				}else{
 					//执行单个队列single_queue_name
-					run_queue(single_queue_name,single_attribute);
+					await run_queue(single_queue_name,single_attribute);
 				}
 				
 			};
 		};
-
-	});
+	};
 }
 
 
 //执行单个队列
-function run_queue(queue_name,attribute){
+async function run_queue(queue_name,attribute){
 	queue_status_array[queue_name] = 1 ;
 	//获取有序集合里的score最小并且score小于当前时间戳的一个元素
 	var cur_time = Date.parse(new Date());
-	redis_client.zrangebyscore(queue_name,0,cur_time,'LIMIT',0,1,function(err, reply){
+	var reply =  await redis_client.zrangebyscore(queue_name,0,cur_time,'LIMIT',0,1);
+	if (reply && reply[0] && reply[0] != '' && reply[0] != 'undefined') {
 		var url = reply[0] ;
-		if (url &&  url!= '' &&  url!= 'undefined' ) {
-			//为了防止redis元素重复，在添加url的时候自动加了些随机数。现在需要去掉随机数才是真正的访问url
-			var request_url = url.substring(0 ,url.indexOf("htq_no_repeat=")-1 );
-			//如果是可变队列
-			if (attribute.type == 'variable') {
-				times_queue = queue_name+"_times";
-				//获取执行次数
-				redis_client.hget(times_queue,url,function(err, reply){
-					var execution_times = parseInt(reply) ? parseInt(reply) : 0 ;
-					request(request_url, function (error, response, body) {
-						////console.log("第"+(execution_times+1)+"次执行来自"+queue_name+"的url："+request_url);
-						if (body == "done") {
-							//删除任务
-							redis_client.zrem(queue_name,url);
-							redis_client.hdel(times_queue,url);
-						}
-						else if (body == "reset") {
-							//重置
-							redis_client.hset(times_queue,url,0);
-							next_time = Date.parse(new Date()) + attribute.stepping_time*1000 ;
-							redis_client.zadd(queue_name,next_time,url);
-							
-						}else{
-							var offset = execution_times*attribute.stepping_time*1000 ;
-							offset = offset > attribute.max_time_interval*1000 ? attribute.max_time_interval*1000 :offset ;
-							next_time = Date.parse(new Date()) + offset ;
-							////console.log("推迟到"+next_time+"执行");
-							redis_client.zadd(queue_name,next_time,url);
-							redis_client.hset(times_queue,url,execution_times+1);
+		//为了防止redis元素重复，在添加url的时候自动加了些随机数。现在需要去掉随机数才是真正的访问url
+		var request_url = url.substring(0 ,url.indexOf("htq_no_repeat=")-1 );
+		//如果是可变队列
+		if (attribute.type == 'variable') {
+			times_queue = queue_name+"_times";
+			//获取执行次数
+			let reply =  await redis_client.hget(times_queue,url) ;
+			if (reply) {
+				var execution_times = parseInt(reply) ? parseInt(reply) : 0 ;
+				let response = await fetch(request_url,{ timeout : 30*1000});
+				let body = await response.text();
 
-						}
-						//queue_status_array[queue_name] = 0 ;
-						run_queue(queue_name,attribute);
-						return ;
-					});
-				});
+				////console.log("第"+(execution_times+1)+"次执行来自"+queue_name+"的url："+request_url);
+				if (body == "done") {
+					//删除任务
+					await redis_client.zrem(queue_name,url);
+					await redis_client.hdel(times_queue,url);
+				}
+				else if (body == "reset") {
+					//重置
+					await redis_client.hset(times_queue,url,0);
+					next_time = Date.parse(new Date()) + attribute.stepping_time*1000 ;
+					await redis_client.zadd(queue_name,next_time,url);
+					
+				}else{
+					var offset = execution_times*attribute.stepping_time*1000 ;
+					offset = offset > attribute.max_time_interval*1000 ? attribute.max_time_interval*1000 :offset ;
+					next_time = Date.parse(new Date()) + offset ;
+					////console.log("推迟到"+next_time+"执行");
+					await redis_client.zadd(queue_name,next_time,url);
+					await redis_client.hset(times_queue,url,execution_times+1);
 
-
-
-			}else{
-			 //如果是实时队列或者定时队列
-				//删除这个元素。不在执行url后在删除是为了防止因为执行不了url而造成阻塞
-				redis_client.zrem(queue_name,url,function(){
-					request(request_url, function (error, response, body) {
-						//console.log("执行来自"+queue_name+"的url："+request_url);
-						//queue_status_array[queue_name] = 0 ;
-						run_queue(queue_name,attribute);
-						return ;
-					});
-				});
-			}
-
-
+				}
+				//queue_status_array[queue_name] = 0 ;
+				await run_queue(queue_name,attribute);
+				return ;
+			};
 
 		}else{
-			//这里是结束递归的关键：不再返回元素，集合为空或者score小于当前时间戳的集合为空
-			queue_status_array[queue_name] = 0 ;
-			return false;
+		 //如果是实时队列或者定时队列
+			//删除这个元素。不在执行url后在删除是为了防止因为执行不了url而造成阻塞
+			await redis_client.zrem(queue_name,url);
+			await fetch(request_url,{ timeout : 30*1000});
+			//console.log(request_url);
+			await run_queue(queue_name,attribute);
+			return ;
 		}
 
-	});	
+	}else{
+		//这里是结束递归的关键：不再返回元素，集合为空或者score小于当前时间戳的集合为空
+		queue_status_array[queue_name] = 0 ;
+		return false;
+	}
+
+
 }
